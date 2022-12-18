a7129de31ff12fb7e28dc9b3062518f06aa36748
Jacob Sanders <sanders.jms@gmail.com>
2022-12-18 16:25:58 -0700

    initial human_player class setup

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,3 +1,15 @@
-class HumanPlayer < Player
+require_relative 'player'
 
+class HumanPlayer < Player
+  attr_reader :name
+  def initialize(name)
+    super
+  end
+
+  def self.create_player
+    puts "Please enter your name:"
+    name = gets.chomp
+    puts "Thanks #{name}, you're all set up!"
+    HumanPlayer.new(name)
+  end
 end
