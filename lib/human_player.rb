83a85331f0bce2f59fd3e11dba7f9292a283ea9e
hawkobe <sanders.jms@gmail.com>
2022-12-19 00:03:39 -0700

    added guess methods for human player

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,9 +1,13 @@
 require_relative 'player'
 
+# need to create a history of guesses, which is essentially the board
+
 class HumanPlayer < Player
-  attr_reader :name
+  COLOR_OPTIONS = ["red", "green", "blue", "yellow", "purple", "cyan"]
+  attr_reader :name, :guess
   def initialize(name)
     super
+    @guess = nil
   end
 
   def self.create_player
@@ -12,4 +16,20 @@ class HumanPlayer < Player
     puts "Thanks #{name}, you're all set up!"
     HumanPlayer.new(name)
   end
+
+  def place_guess
+    puts "#{@name} Please make your four color guess separated by spaces with no punctuation:"
+    @guess = gets.chomp.downcase.split
+    until guess_valid?(guess)
+      puts "Invalid guess, please select a valid guess:"
+      @guess = gets.chomp.downcase.split
+    end
+    puts "Your guess is: | #{@guess[0]} | #{@guess[1]} | #{@guess[2]} | #{@guess[3]} |"
+  end
+
+  def guess_valid?(guess)
+    guess.all? { |color| COLOR_OPTIONS.any?(color) }
+  end
 end
+
+jacob = HumanPlayer.new("Jacob").place_guess
