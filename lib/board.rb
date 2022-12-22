1ee4268096422671ec002121947babd32743e9d3
hawkobe <sanders.jms@gmail.com>
2022-12-22 13:27:03 -0700

    started a display board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,12 @@
 # look into Gosu for GUI version of board
 
 class Board
+  attr_accessor :board
   def initialize
-    @board = 
+    @board = []
   end
 
   def display_board
+    @board.each {|guess_array| p guess_array}
   end
-  
 end
