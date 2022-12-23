21a6a0fd712e8afb2d76c7acb99fc9e1392c1c2d
hawkobe <sanders.jms@gmail.com>
2022-12-23 03:01:25 -0700

    moved populate matches array and push guess methods to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,23 @@
 # look into Gosu for GUI version of board
 
 class Board
-  attr_accessor :board
   def initialize
     @board = []
+    @matches = []
   end
 
   def display_board
-    @board.each {|guess_array| p "#{guess_array[0]} | #{guess_array[1]} | #{guess_array[2]} | #{guess_array[3]} |"}
+    @board.each_with_index do |guess_array, index| 
+      print "#{guess_array[0]} | #{guess_array[1]} | #{guess_array[2]} | #{guess_array[3]} |"
+      puts " Exact matches: #{@matches[index][0]}. Partial Matches: #{@matches[index][1]}"
+    end
+  end
+
+  def populate_matches_array(exact, partial)
+    @matches.push([exact, partial])
+  end
+
+  def push_guess(guess)
+    @board.push(guess)
   end
 end
