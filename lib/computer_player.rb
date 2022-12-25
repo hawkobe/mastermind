0f7703065c9e89375a9bb03385bb18792cb1044f
hawkobe <sanders.jms@gmail.com>
2022-12-25 01:03:28 -0700

    started algorithm for comp to make guesses. it hard

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -4,4 +4,16 @@ class ComputerPlayer < Player
   def initialize(name)
     super
   end
+  
+  def determine_possible_combinations
+    possible_combos = COLOR_OPTIONS.repeated_permutation(4).to_a
+  end
+
+  # need to write algorithm that lets computer make educated guesses
+  # thinking along the lines of:
+  
+  # new_possibles = possible_combos.select { |array| array.count("red") <= 1 && array.count("green") <= 1}
+  
+  # based on first guess being "red red green green"
+  # and partial matches = 1
 end
