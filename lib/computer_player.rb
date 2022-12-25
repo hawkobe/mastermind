69cf461f146e71aaaea71d9f6506d2349e650868
hawkobe <sanders.jms@gmail.com>
2022-12-25 12:34:29 -0700

    updating notes for algorithm

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -9,11 +9,26 @@ class ComputerPlayer < Player
     possible_combos = COLOR_OPTIONS.repeated_permutation(4).to_a
   end
 
-  # need to write algorithm that lets computer make educated guesses
+  # red = 1 green = 2 blue = 3 yellow = 4 purple = 5 cyan = 6
+
+  # need to write algorithm that lets computer make educated guesses.
   # thinking along the lines of:
   
-  # new_possibles = possible_combos.select { |array| array.count("red") <= 1 && array.count("green") <= 1}
+  # if only partials and no exact matches:
+  # use number of partials to determine "n"
+  # new_possibles = possible_combos.select do |array| 
+  #                   (array.count("red") <= n && array[0] != "red" && array[1] != "red") && (array.count("green") <= n && array[2] != "green" && array[3] != "green"
+
   
   # based on first guess being "red red green green"
-  # and partial matches = 1
+
+  # will want computer to guess the lowest possible guess
+  # from new possibles (e.g. [2, 3, 4, 5] over [3, 4, 5, 6])
+
+  # possible outcomes of initial r r g g guess:
+  # 1: four exact matches (win, so don't have to include)
+  # 2: zero exact matches zero partials (eliminate those colors entirely from new possibles code)
+  # 3: zero exact matches and 1 partial (if no exact, then -> [line 17])
+  # 4: zero exact matches and 2 partial (if no exact, then -> [line 17])
+
 end
