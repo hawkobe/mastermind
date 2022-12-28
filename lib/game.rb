e53bed357519c89f6ee54540d9e60944b8a2e41d
hawkobe <sanders.jms@gmail.com>
2022-12-28 15:13:38 -0700

    refactored game by moving relevant methods to computer_player

--- lib/game.rb
+++ lib/game.rb
@@ -9,24 +9,21 @@ class Game
   COLOR_OPTIONS = ["red", "green", "blue", "yellow", "purple", "cyan"]
   def initialize
     @board = Board.new
-    @code = []
-    @players = []
+    @human = nil
+    @computer = nil
     @current_player = nil
-    @exact_matches = 0
-    @partial_matches = 0
   end
 
   def setup
     welcome_message
     create_players
-    assign_code
+    @computer.assign_code
     breaker_message
-    @current_player = @players[0]
   end
 
   def play
     setup
-    game_loop
+    player_guess_loop
   end
 
   def welcome_message
@@ -42,60 +39,26 @@ class Game
   end
 
   def create_players
-    @players << HumanPlayer.create_player
-    @players << ComputerPlayer.new("Computer")
-    @current_player = @players[0]
-  end
-  
-  def assign_matches
-    matched = []
-    partially_matched = []
-    @current_player.guess.each_with_index do |color, index|
-      if @current_player.guess[index] == @code[index]
-        @exact_matches += 1
-        matched.push(color)
-      elsif code.any?(color) && matched.count(color) != @code.count(color) && partially_matched.count(color) != @code.count(color)
-        @partial_matches += 1
-        partially_matched.push(color)
-      end
-
-      if (matched.count(color) == partially_matched.count(color)) && ((matched.count(color) + partially_matched.count(color)) > code.count(color))
-        @partial_matches -= 1
-      end
-      binding.pry
-    end
+    @human = HumanPlayer.create_player
+    @computer = ComputerPlayer.new("Computer")
+    @current_player = @human
   end
 
-  def game_loop
+  def player_guess_loop
     12.times do
       @current_player.place_guess
       @board.push_guess(@current_player.guess)
-      assign_matches
-      @board.populate_matches_array(@exact_matches, @partial_matches)
-      break end_game if game_won?
+      @computer.assign_matches(@human)
+      @board.populate_matches_array(@computer.matches)
+      break end_game if @computer.game_won?(@human)
       @current_player.clear_guess
-      clear_matches
+      @computer.clear_matches
       @board.display_board
     end
   end
 
-  def clear_matches
-    @exact_matches = 0
-    @partial_matches = 0
-  end
-
-  def game_won?
-    @current_player.guess == @code
-  end
-
   def end_game
     puts "You have guessed the code! YOU are the Mastermind!"
-  end
-
-  # private
-  attr_reader :code, :players
-  def assign_code
-    4.times { @code.push(COLOR_OPTIONS.sample) }
   end
 end
 
